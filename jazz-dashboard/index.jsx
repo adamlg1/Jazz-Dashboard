0e469146dd14190bc4f2ab860a0cb376d8ce9898
adamlg1 <80432653+adamlg1@users.noreply.github.com>
2025-04-06 23:18:58 -0600

    create about page

--- jazz-dashboard/index.jsx
+++ jazz-dashboard/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import JazzStatsDashboard from './components/JazzStatsDashboard';
 import Login from './components/login';
+import About from './components/about';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
@@ -16,8 +17,10 @@ root.render(
             <Header />
             <Routes>
                 <Route path="/" element={<JazzStatsDashboard />} />
-                <Route path="/stats" element={<JazzStatsDashboard />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/about" element={<About />} />  {/* Add route for About page */}
+                <Route path="/stats" element={<JazzStatsDashboard />} />
+
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
